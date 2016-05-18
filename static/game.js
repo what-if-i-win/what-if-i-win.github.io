e0e3d442347262362eff6f9c1f7c83752b08e105
정경업 <py@smartstudy.co.kr>
2016-05-18 11:54:39 +0900

    first commit

---
+++ static/game.js
@@ -0,0 +1,54 @@
+var items = ['rock', 'paper', 'scissors'];
+var RESULTS = [];
+var CURRENT = localStorage.getItem("current") !== null ? localStorage.getItem("current") : 0;
+console.log(CURRENT);
+
+function clearClass(selector) {
+    for (var idx in items) {
+        $(selector).removeClass(items[idx]);
+    }
+}
+
+function updateCurrent() {
+    clearClass('#current');
+    $('#current').removeClass(['rock', 'paper', 'scissors']).addClass(RESULTS[CURRENT]);
+    console.log(CURRENT);
+    $('#results .tile').eq(CURRENT).addClass('active').siblings().removeClass('active');
+}
+
+function updateList() {
+    clearClass('#results .tile');
+    for (var idx in RESULTS) {
+        var name = RESULTS[idx];
+        $('#results .tile').eq(idx).addClass(name);
+    }
+}
+
+function refreshList() {
+    RESULTS = [];
+    for (var i = 0; i < 5; i++) {
+        var rand = items[Math.floor(Math.random() * items.length)];
+        RESULTS.push(rand);
+    }
+    localStorage.setItem("current", 0);
+    CURRENT = 0;
+    console.log(RESULTS);
+
+    updateList();
+    updateCurrent();
+}
+
+$('#next').click(function () {
+    CURRENT++;
+    CURRENT = CURRENT > 4 ? 0 : CURRENT;
+    updateCurrent();
+});
+$('#prev').click(function () {
+    CURRENT--;
+    CURRENT = CURRENT < 0 ? 4 : CURRENT;
+    updateCurrent();
+});
+
+$('#refresh').click(function () {
+    refreshList();
+}).trigger('click');
